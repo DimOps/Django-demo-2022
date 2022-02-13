93e3f36fd86ee066724d91fd0efa1fac3054db5c
Dimitar Dzhapunov <90458809+DimOps@users.noreply.github.com>
2022-02-13 22:27:02 +0200

    Include like logic

--- petstagram/petstagram/main/urls.py
+++ petstagram/petstagram/main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from petstagram.main.views import display_home, display_dashboard, display_profile_details, display_photo_details
-
+from petstagram.main.views import display_home, display_dashboard, display_profile_details, display_photo_details, \
+    like_photo
 
 urlpatterns = (
     path('', display_home, name='index'),
     path('dashboard/', display_dashboard, name='dashboard'),
     path('profile/', display_profile_details, name='profile'),
     path('photo/details/<int:pk>/', display_photo_details, name='photo'),
+    path('photo/like/<int:pk>/', like_photo, name='like photo'),
 )
--- petstagram/petstagram/main/views.py
+++ petstagram/petstagram/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from petstagram.main.models import Profile, PetPhoto
 
@@ -10,6 +10,7 @@ def get_profile():
     else:
         return None
 
+
 def display_home(request):
     context = {
         'hide_nav_items': True,
@@ -19,15 +20,27 @@ def display_home(request):
 
 def display_dashboard(request):
     profile = get_profile()
-    pet_photos = PetPhoto.objects.filter(tagged_animals__user_profile=profile)
+    pet_photos = set(PetPhoto.objects.filter(tagged_animals__user_profile=profile))
     context = {
         'pet_photos': pet_photos
     }
     return render(request, 'dashboard.html', context)
 
 
-def display_photo_details(request):
-    return render(request, 'photo_details.html')
+def display_photo_details(request, pk):
+    pet_photo = PetPhoto.objects.get(pk=pk)
+
+    context = {
+        'pet_photo': pet_photo,
+    }
+    return render(request, 'photo_details.html', context)
+
+
+def like_photo(request, pk):
+    pet_photo = PetPhoto.objects.get(pk=pk)
+    pet_photo.likes += 1
+    pet_photo.save()
+    return redirect('photo', pk)
 
 
 def display_profile_details(request):
