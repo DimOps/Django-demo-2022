b15cc882c08130a87e11c6ff009ddc90ebec910e
Dimitar Dzhapunov <90458809+DimOps@users.noreply.github.com>
2022-02-13 17:13:25 +0200

    Amend templates
    Include logic for the homepage template.

--- petstagram/petstagram/main/views.py
+++ petstagram/petstagram/main/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
 
+from petstagram.main.models import Profile, PetPhoto
+
+
+def get_profile():
+    profiles = Profile.objects.all()
+    if profiles:
+        return profiles[0]
+    else:
+        return None
 
 def display_home(request):
-    return render(request, 'home_page.html')
+    context = {
+        'hide_nav_items': True,
+    }
+    return render(request, 'home_page.html', context)
 
 
 def display_dashboard(request):
-    return render(request, 'dashboard.html')
+    profile = get_profile()
+    pet_photos = PetPhoto.objects.filter(tagged_animals__user_profile=profile)
+    context = {
+        'pet_photos': pet_photos
+    }
+    return render(request, 'dashboard.html', context)
 
 
 def display_photo_details(request):
