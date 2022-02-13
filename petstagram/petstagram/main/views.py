6909f137875c9024ff64db159132deaec1d7e9a6
Dimitar Dzhapunov <90458809+DimOps@users.noreply.github.com>
2022-02-14 01:33:23 +0200

    Profile template functionality
    Likes and images count added.

--- petstagram/petstagram/main/views.py
+++ petstagram/petstagram/main/views.py
@@ -44,7 +44,18 @@ def like_photo(request, pk):
 
 
 def display_profile_details(request):
-    return render(request, 'profile_details.html')
+    profile = get_profile()
+    pet_photos = PetPhoto.objects.filter(tagged_animals__user_profile=profile).distinct()
+
+    total_user_likes = sum(pp.likes for pp in pet_photos)
+    total_user_pets_photos = len(pet_photos)
+
+    context = {
+        'profile': profile,
+        'total_user_likes': total_user_likes,
+        'total_user_pets_photos': total_user_pets_photos,
+    }
+    return render(request, 'profile_details.html', context)
 
 
 def display_forbidden(request):
